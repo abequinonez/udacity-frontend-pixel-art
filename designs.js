15f484e3d6a5964a6eb08f93c47bbc956bec31a3
abequinonez <abequinonez@users.noreply.github.com>
2017-09-03 03:49:11 -0700

    Added event listener to table cells

--- designs.js
+++ designs.js
@@ -18,30 +18,38 @@ const table = document.getElementById('pixel_canvas');
 
 // When size is submitted by the user, call makeGrid()
 submitButton.addEventListener('click', function() {
-	makeGrid(gridHeight.value, gridWidth.value);
+    makeGrid(gridHeight.value, gridWidth.value);
 });
 
 // Make an N by M grid based on the grid size inputs
 function makeGrid(height, width) {
-	// Clear the grid, if one exists
-	if (table.innerHTML.length > 0) {
-		table.innerHTML = '';
-	}
-	for (let i = 0; i < height; i++) {
-		/*
-		Create a table row. MDN used as a reference for the createElement()
-		and appendChild() methods.
-		*/
-		let row = document.createElement('tr');
+    // Clear the grid, if one exists
+    if (table.innerHTML.length > 0) {
+        table.innerHTML = '';
+    }
+    for (let i = 0; i < height; i++) {
+        /*
+        Create a table row. MDN used as a reference for the createElement()
+        and appendChild() methods.
+        */
+        let row = document.createElement('tr');
 
-		// Append the row to the table
-		table.appendChild(row);
-		for (let j = 0; j < width; j++) {
-			// Create a table cell
-			let col = document.createElement('td');
+        // Append the row to the table
+        table.appendChild(row);
+        for (let j = 0; j < width; j++) {
+            // Create a table cell
+            let col = document.createElement('td');
 
-			// Append the cell to the row
-			row.appendChild(col);
-		}
-	}
+            /*
+            Add an event listener to each cell. When clicked, the cell's background
+            color will change to the current value of the color picker.
+            */
+            col.addEventListener('click', function() {
+                col.style.backgroundColor = colorPicker.value;
+            });
+
+            // Append the cell to the row
+            row.appendChild(col);
+        }
+    }
 }

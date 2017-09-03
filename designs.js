c1769ac22eb175c8bd72a7fb9ccd5f9f2f32c9bf
abequinonez <abequinonez@users.noreply.github.com>
2017-09-03 15:37:32 -0700

    Changed input to button element

--- designs.js
+++ designs.js
@@ -1,24 +1,25 @@
-/*
-Select the submit button. I used querySelector in order to select the
-button by its type attribute, as learned from the following Stack
-Overflow discussion:
-https://stackoverflow.com/questions/5897122/accessing-elements-by-type-on-javascript
-*/
-const submitButton = document.querySelector('input[type=submit]');
-
-// Select the color picker
-const colorPicker = document.getElementById('colorPicker');
-
 // Select the grid size inputs
 const gridHeight = document.getElementById('input_height');
 const gridWidth = document.getElementById('input_width');
 
+// Select the make grid button
+const makeGridBtn = document.getElementsByTagName('button')[0];
+
+// Select the color picker
+const colorPicker = document.getElementById('colorPicker');
+
 // Select the table
 const table = document.getElementById('pixel_canvas');
 
-// When size is submitted by the user, call makeGrid()
-submitButton.addEventListener('click', function() {
-    makeGrid(gridHeight.value, gridWidth.value);
+// When the make grid button is clicked, call makeGrid()
+makeGridBtn.addEventListener('click', function() {
+    const height = gridHeight.value;
+    const width = gridWidth.value;
+
+    // Only call makeGrid() if both height and width are greater than 0
+    if (height > 0 && width > 0) {
+        makeGrid(height, width);
+    }
 });
 
 // Make an N by M grid based on the grid size inputs
@@ -32,13 +33,13 @@ function makeGrid(height, width) {
         Create a table row. MDN used as a reference for the createElement()
         and appendChild() methods.
         */
-        let row = document.createElement('tr');
+        const row = document.createElement('tr');
 
         // Append the row to the table
         table.appendChild(row);
         for (let j = 0; j < width; j++) {
             // Create a table cell
-            let col = document.createElement('td');
+            const col = document.createElement('td');
 
             /*
             Add an event listener to each cell. When clicked, the cell's background

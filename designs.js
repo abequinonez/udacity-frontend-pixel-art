a244215eb972e36c36a67ca48b28e602f11fd6d0
abequinonez <abequinonez@users.noreply.github.com>
2017-09-02 17:10:19 -0700

    Added variables containing selected DOM elements

--- designs.js
+++ designs.js
@@ -1,10 +1,33 @@
-// Select color input
-// Select size input
+/*
+Select the submit button. I used querySelector in order to select the
+button by its type attribute, as learned from the following Stack
+Overflow discussion:
+https://stackoverflow.com/questions/5897122/accessing-elements-by-type-on-javascript
+*/
+const submitButton = document.querySelector('input[type=submit]');
+
+// Select the color picker
+const colorPicker = document.getElementById('colorPicker');
+
+// Select the grid size inputs
+const gridHeight = document.getElementById('input_height');
+const gridWidth = document.getElementById('input_width');
+
+// Select the table
+const table = document.getElementById('pixel_canvas');
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
+function makeGrid(width, height) {
 
 // Your code goes here!
+	for (let i = 0; i < height; i++) {
+		let row = document.createElement('tr');
+		table.appendChild(row);
+		for (let j = 0; j < width; j++) {
+			let col = document.createElement('td');
+			row.appendChild(col);
+		}
+	}
 
 }
